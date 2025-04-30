08d98ddb47da5421dd473b522650ea51622474f4
Akadirr1 <akadirr41@outlook.com>
2025-04-30 22:05:41 +0300

    admin update

--- Library.c
+++ Library.c
@@ -88,7 +88,7 @@ void registerUser()
 		user.isAdmin = false;
 
 		FILE *fp;
-		FILE *Idp = fopen("UniqueID.dat", "r");
+		FILE *Idp = fopen("UniqueUserID.dat", "r");
 		if (Idp == NULL)
 		{
 			printf("Dosya acilamadi");
@@ -101,7 +101,7 @@ void registerUser()
 			ID++;
 			fclose(Idp);
 		}
-		Idp = fopen("UniqueID.dat", "w");
+		Idp = fopen("UniqueUserID.dat", "w");
 		if (Idp == NULL)
 		{
 			printf("Dosya acilamadi");
@@ -120,7 +120,8 @@ void registerUser()
 		printf("User registered successfully!\n");
 	}
 }
-void loginUser(int *islogin,int *isAdmin){
+void loginUser(int *islogin, int *isAdmin)
+{
 	char email[250];
 	char password[250];
 	bool emailFound = false;
@@ -130,10 +131,10 @@ void loginUser(int *islogin,int *isAdmin){
 
 	printf("Enter your password: ");
 	fgets(password, sizeof(password), stdin);
-	
+
 	email[strcspn(email, "\n")] = 0;
-    password[strcspn(password, "\n")] = 0;
-    
+	password[strcspn(password, "\n")] = 0;
+
 	const char delimiter[2] = ",";
 	char buffer[512];
 
@@ -143,7 +144,7 @@ void loginUser(int *islogin,int *isAdmin){
 		printf("Dosya acilamadi");
 		return;
 	}
-	*islogin=0;
+	*islogin = 0;
 	while (fgets(buffer, sizeof(buffer), fp) != NULL)
 	{
 		buffer[strcspn(buffer, "\n")] = 0;
@@ -157,45 +158,102 @@ void loginUser(int *islogin,int *isAdmin){
 		char *storedPassword = strtok(NULL, delimiter);
 		char *StoredAdmin = strtok(NULL, delimiter);
 
-
 		if (storedEmail != NULL && storedPassword != NULL)
 		{
 			storedEmail[strcspn(storedEmail, "\n")] = 0;
 			storedPassword[strcspn(storedPassword, " \n")] = 0;
 
-			if (strcmp(storedEmail, email) == 0){
+			if (strcmp(storedEmail, email) == 0)
+			{
 				emailFound = true;
-				if(strcmp(storedPassword, password)==0)
+				if (strcmp(storedPassword, password) == 0)
 				{
-					*islogin=1;
-					*isAdmin=atoi(StoredAdmin);
+					*islogin = 1;
+					*isAdmin = atoi(StoredAdmin);
 					break;
 				}
-				else{
+				else
+				{
 					break;
 				}
 			}
 		}
 	}
 	fclose(fp);
-	if (!emailFound) {
-        printf("Email not found\n");
-    } else if (*islogin == 0) {
-        printf("Password is incorrect\n");
-    }
+	if (!emailFound)
+	{
+		printf("Email not found\n");
+	}
+	else if (*islogin == 0)
+	{
+		printf("Password is incorrect\n");
+	}
+}
+void addBook(){
+	Book book;
+	printf("Enter book name: ");
+	fgets(book.bookName, sizeof(book.bookName), stdin);
+	printf("Enter author name: ");
+	fgets(book.author, sizeof(book.author), stdin);
+	printf("Enter category: ");
+	fgets(book.ctgry, sizeof(book.ctgry), stdin);
+	book.bookName[strcspn(book.bookName, "\n")] = 0;
+	book.author[strcspn(book.author, "\n")] = 0;
+	book.ctgry[strcspn(book.ctgry, "\n")] = 0;
+	book.taken = false;
+	book.userId = -1;
+	book.borrowDate[0] = '\0';
+	book.returnDate[0] = '\0';
+	FILE *fp;
+	FILE *Idp = fopen("UniqueBookID.dat", "r");
+	if (Idp == NULL)
+	{
+		printf("Dosya acilamadi");
+		return;
+	}
+	else
+	{
+		book.bookId = ID;
+		ID++;
+		fclose(Idp);
+	}
+	
+}
+void adminChoose(int choice)
+{
+	switch (choice)
+	{
+	case 1:
+		registerUser(); 
+		break;
+	case 2:
+		// Add Book
+		break;
+	case 3:
+		// Update Book
+		break;
+	case 4:
+		// Delete Book
+		break;
+	case 5:
+		// View outDate Books
+		break;
+	case 6:
+		printf("Logging out...\n");
+		break;
+	default:
+		printf("Invalid choice. Please try again.\n");
+	}
 }
 
-
-
-
 int main()
-{	
-	int islogin= 0;
-	int isAdmin=0;
+{
+	int islogin = 0;
+	int isAdmin = 0;
 	int choice = 0;
 	char choiceStr[10];
 	while (choice != 3)
-	{	
+	{
 		printf("Welcome to the Library System\n");
 		printf("1. Register\n");
 		printf("2. Login\n");
@@ -208,23 +266,31 @@ int main()
 			continue;
 		}
 		else if (choice == 2)
-		{	
-			loginUser(&islogin,&isAdmin);
-			if(islogin==1)
+		{
+			loginUser(&islogin, &isAdmin);
+			if (islogin == 1)
 			{
 				printf("Login successful!\n");
 				printf("Welcome to the library system!\n");
-				if(isAdmin==1)
+				if (isAdmin == 1)
 				{
-					printf("You are an admin\n");
-					
+					printf("You are an admin\n choose the admin functions\n");
+					printf("1. Add User\n");
+					printf("2. Add Book\n");
+					printf("3. Update Book\n");
+					printf("4. Delete Book\n");
+					printf("5. View outDate Books\n");
+					printf("6. Logout\n");
+					int adminChoice;
+					fgets(choiceStr, sizeof(choiceStr), stdin);
+					adminChoice = atoi(choiceStr);
+					adminChoose(adminChoice);
 				}
 				else
 				{
 					printf("You are a user\n");
 					// User functions can be called here
 				}
-
 			}
 			else
 			{
